4f5a05c4320b4c0f5691fab0b03082f129914f58
SerVa2 <129479148+SerVa2@users.noreply.github.com>
2023-04-11 13:50:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1633714108 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
@@ -11,4 +11,19 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    alpha = 0.05 # уровень значимости
+    
+    p_control = x_success / x_cnt # вероятность успеха на выборке из контроля
+    p_test = y_success / y_cnt # вероятность успеха на выборке из теста
+    
+    p_combined = (x_success + y_success) / (x_cnt + y_cnt) # объединенная вероятность успеха
+    se = np.sqrt(p_combined * (1 - p_combined) * (1/x_cnt + 1/y_cnt)) # стандартная ошибка разности
+    
+    z_score = (p_test - p_control) / se # Z-оценка
+    
+    if z_score > 0 and z_score > alpha/2: # если Z-оценка больше уровня значимости и положительна
+        return True # отклоняем нулевую гипотезу
+    elif z_score < 0 and abs(z_score) > alpha/2: # если Z-оценка меньше уровня значимости и отрицательна
+        return True # отклоняем нулевую гипотезу
+    else:
+        return False # не отклоняем нулевую гипотезу
