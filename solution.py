79fc80f828bf1ee7967b792cd62a71d1fe372f24
SerVa2 <129479148+SerVa2@users.noreply.github.com>
2023-04-13 18:17:14 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -14,6 +14,6 @@ def solution(x_success: int,
     # Измените код этой функции
     # Это будет вашим решением
     # Не меняйте название функции и её аргументы
-    alpha = 0.02
+    alpha = 0.05
     stats, p_value = proportions_ztest(np.array([x_success, y_success]), np.array([x_cnt, y_cnt]), alternative = 'smaller')
     return p_value < alpha
